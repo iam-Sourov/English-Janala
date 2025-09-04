a446a7cf7a68baa8acf80ff9e241aaa1507d088d
Sourov-Ahmed26 <silenboyk7@gmail.com>
2025-09-04 23:00:41 +0600

    btn stylings

--- script.js
+++ script.js
@@ -48,17 +48,17 @@ const displayWords = (words) => {
     words.forEach(word => {
         const card = document.createElement('div');
         card.innerHTML = `
-                <div class="card-body  w-80 bg-[#FFFFFF] rounded shadow-md flex justify-center gap-2 p-10 text-center">
+                <div class="card-body w-full  md:w-80 bg-[#FFFFFF] rounded shadow-md flex justify-center gap-2 p-10 text-center">
                     <h2 class="card-title-center text-2xl">
-                        ${word.word ? word.word :"No Word Is Found"}
+                        ${word.word ? word.word : "No Word Is Found"}
                     </h2>
                     <p class="text-md">Meaning /Pronounciation</p>
-                    <p class="text-2xl">${word.meaning ? word.meaning:"No Meaning Is Found"}/${word.pronunciation}</p>
-                    <div class= flex justify-between items-center">
-                        <div class="bg-[#1A91FF1A] w-14 h-14 rounded-lg flex justify-center items-center"><i
+                    <p class="text-2xl">${word.meaning ? word.meaning : "No Meaning Is Found"}/${word.pronunciation}</p>
+                    <div class="flex justify-between items-center">
+                        <div class="btn btn-soft btn-primary hover:bg-primary bg-[#1A91FF1A] w-14 h-14 rounded-lg flex justify-center items-center"><i
                                 class="fa-solid fa-circle-info"></i></div>
-                        <div class="bg-[#1A91FF1A] w-14 h-14 rounded-lg flex justify-center items-center"><i
-                                class="fa-solid fa-volume-high"></i></div>
+                        <div class="btn btn-soft btn-primary hover:bg-primary bg-[#1A91FF1A] w-14 h-14 rounded-lg flex justify-center items-center"><i
+                                class=" fa-solid fa-volume-high"></i></div>
                     </div>
                 </div>
         `
