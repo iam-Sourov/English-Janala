cb788dd0b27a55d167ebbd8481ae3c96913d54b5
Sourov-Ahmed26 <silenboyk7@gmail.com>
2025-09-04 22:50:14 +0600

    Structuring Part Two

--- script.js
+++ script.js
@@ -31,16 +31,29 @@ const displayWords = (words) => {
     const wordContainer = document.getElementById('word-container');
     wordContainer.innerHTML = '';
 
+
+    if (words.length == 0) {
+        wordContainer.innerHTML = `
+        <div id="info" class=" col-span-3  bg-[#F8F8F8] p-8 m-2 space-y-4">
+            <img class="mx-auto" src="assets/alert-error.png" alt="">
+            <p>এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+            <h1 class="text-2xl">নেক্সট Lesson এ যান</h1>
+        </div>
+        `;
+        return;
+    }
+
+
+
     words.forEach(word => {
-        console.log(word);
         const card = document.createElement('div');
         card.innerHTML = `
                 <div class="card-body  w-80 bg-[#FFFFFF] rounded shadow-md flex justify-center gap-2 p-10 text-center">
                     <h2 class="card-title-center text-2xl">
-                        ${word.word}
+                        ${word.word ? word.word :"No Word Is Found"}
                     </h2>
                     <p class="text-md">Meaning /Pronounciation</p>
-                    <p class="text-2xl">${word.meaning}/${word.pronunciation}</p>
+                    <p class="text-2xl">${word.meaning ? word.meaning:"No Meaning Is Found"}/${word.pronunciation}</p>
                     <div class= flex justify-between items-center">
                         <div class="bg-[#1A91FF1A] w-14 h-14 rounded-lg flex justify-center items-center"><i
                                 class="fa-solid fa-circle-info"></i></div>
